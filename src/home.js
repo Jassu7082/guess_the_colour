c2d26d785bcc0f560fdc4aafa76ba04afdd7f8c8
Jaswanth <Jaswanth7082@gmail.com>
2024-02-02 23:15:30 +0530

    made the buttons to center

--- src/home.js
+++ src/home.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from './logo.png';
-import { Link} from 'react-router-dom'; // Import the useHistory hook
+import { Link} from 'react-router-dom';
 import './home.css';
 
 function Home() {
     return (
         <div className='whloe-div'>
-            <header className="App-header">
+            <div className="App-header">
                 <img className="logo" src={logo} alt="logo" />
-            </header>
+            </div>
+            <div className="hover1">
+
+            </div>
             <div className="button-container">
-                <Link to="/sp" className="minecraft-button">SinglePlayer</Link>
+                <Link to="/sp" className="minecraft-button single-player">SinglePlayer</Link>
             
-                <Link to="/mp" className="minecraft-button">Multiplayer</Link>
-                {/* <button className="minecraft-button" onClick={}>Stop Server and Go Back</button>
-                 */}
-                <Link to="/" className="minecraft-button">Exit</Link>
+                <Link to="/mp" className="minecraft-button multi-player">Multiplayer</Link>
+
+                <Link to="/" className="minecraft-button third-button">Exit</Link>
             </div>
         </div>
     );
